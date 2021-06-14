6ed4e813ea449dec4722d86ad072b5c3cf2c1468
Dean Cook <deancook371@gmail.com>
2021-06-14 09:46:13 -0600

    Get data from API calls

--- src/api-calls.js
+++ src/api-calls.js
@@ -26,11 +26,78 @@
 //
 // export {retrieveData, postData};
 
-const fetch = require("fetch");
+
+
+
+
+
+// const fetchCustomersData = () => {
+//   return fetch('http://localhost:3001/api/v1/customers')
+//     .then(response => response.json())
+//     .catch(err => console.error("not working"))
+// }
+// const fetchBookingsData = () => {
+//   return fetch('http://localhost:3001/api/v1/bookings')
+//     .then(response => response.json())
+//     .catch(err => console.error("not working"))
+// }
+//
+// const fetchRoomsData = () => {
+//   return fetch('http://localhost:3001/api/v1/rooms')
+//     .then(response => response.json())
+//     .catch(err => console.error("not working"))
+// }
+//
+// const fetchUser = (id) => {
+//   return fetch(`http://localhost:3001/api/v1/customers/${id}`)
+//     .then(response => response.json())
+//     .catch(err => console.error("not working"))
+// }
+//
+// const postBooking = (newBooking) => {
+//   let url = 'http://localhost:3001/api/v1/bookings/'
+//   fetch(url, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(newBooking)
+//   })
+//   .then(checkResponse);
+// }
+//
+// const checkResponse = (response) => {
+//   if (response.ok) {
+//     // document.getElementById('errorMessage').innerText = ""
+//     return response.json();
+//   }
+//   else {
+//     console.log(response.status)
+//     if (response.status === 422) {
+//       let error = new Error('Bad Post Request')
+//       // let message = `${response.status} ${error.message} - Please enter data correctly`
+//       // document.getElementById('errorMessage').innerText = message
+//       throw error;
+//     }
+//     else {
+//       let error = new Error('Something went wrong')
+//       let message = `${error.message}`;
+//       document.getElementById('errorMessage').innerText = message
+//       throw error
+//     }
+//   }
+// }
+//
+// const getData = () => {
+//   return Promise.all([fetchCustomersData(), fetchBookingsData(), fetchRoomsData()])
+// }
+// export default { getData, fetchUser, postBooking }
+
+
+
+
 
 let getData = (endpoint) => fetch(`http://localhost:3001/api/v1/${endpoint}`)
     .then(response => response.json())
-    .catch(err => console.log('error1', err));
+    .catch(err => console.error('not working', err));
 
 function retrieveData() {
   return Promise.all([getData('customers'), getData('rooms'), getData('bookings')]);
--- src/scripts.js
+++ src/scripts.js
@@ -3,12 +3,33 @@ import './css/base.scss';
 
 import './images/turing-logo.png';
 
+import {retrieveData} from './api-calls';
+import Customer from './classes/customer';
+import Booking from './classes/booking';
+import Room from './classes/room';
+
 const bookingsSection = document.querySelector("#bookings");
 const customerInfoSection = document.querySelector("#customerInfo");
 
 //for It1
 // Load random user and assign currentUser to that
 // Display all bookings and amount spent
+
+let currentUser;
+
+window.onload = () => {
+  retrieveData()
+    .then((promise) => {
+      let customerData = promise[0].customers.map((customer) => new Customer(customer));
+      let bookingData = promise[2].bookings.map((booking) => new Booking(booking));
+      let roomData = promise[1].rooms.map((room) => new Room(room));
+      startApp(customerData, bookingData, roomData);
+    });
+}
+
+function startApp(customerData, bookingData, roomData) {
+  //console.log(customerData.length);
+}
 
 //for It2
 // Allow user to select date they'd like to stay
